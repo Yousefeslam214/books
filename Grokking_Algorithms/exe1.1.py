b3e0f4354da6e5ff6cd1594b331cd314d3d90d2a
Yousef eslam <yousefeslam214@gmail.com>
2023-12-23 08:41:37 +0200

    binary_search

---
+++ Grokking_Algorithms/exe1.1.py
@@ -0,0 +1,6 @@
+# 1.1 Suppose you have a sorted list of 128 names, and you’re searching
+# through it using binary search. What’s the maximum number of
+# steps it would take?
+
+
+# log2(128) = 7
---
+++ Grokking_Algorithms/exe1.2.py
@@ -0,0 +1,5 @@
+# 1.2 Suppose you double the size of the list. What’s the maximum
+# number of steps now?
+
+
+# log2(256) = 8
